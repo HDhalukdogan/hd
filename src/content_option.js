a8a2f23d46f7e593aa213570dafb569f04a54e47
haluk.dogan@dmo.gov.tr <haluk.dogan@dmo.gov.tr>
2023-06-05 14:45:22 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "HALUK";
+const meta = {
+    title: "Haluk Doğan",
+    description: "I’m Haluk Doğan Full stack devloper,currently working in Ankara",
+};
+
+const introdata = {
+    title: "I’m Haluk Doğan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Dotnet React Angular",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I'am a computer engineer.",
+};
+const worktimeline = [{
+        jobtitle: "Software Developer",
+        where: "Ankara",
+        date: "2021",
+    },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "Jamalya",
+    //     date: "2019",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "Dotnet",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 65,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Angular",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 55,
+    },
+];
+
+const services = [{
+        title: "WEB",
+        description: "Dotnet React Angular",
+    },
+    {
+        title: "Mobile",
+        description: "React Native",
+    },
+    {
+        title: "Sql",
+        description: "Mssql Oracle Postgre",
+    },
+];
+
+const dataportfolio = [{
+        img: "images/Reactivities.png",
+        description: "Angular-React activity WEBSITE.",
+        link: "https://github.com/HDhalukdogan/Reactivities",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hd.halukdogan@hotmail.com",
+    YOUR_FONE: "(5xx)xxx-xxxx",
+    description: "Contact Me ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/HDhalukdogan",
+    //facebook: "https://facebook.com",
+    //linkedin: "https://linkedin.com",
+    //twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
